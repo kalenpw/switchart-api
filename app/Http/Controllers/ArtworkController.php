1f2dfe0bb825b84eeb60eeb1c058113dcfe7782d
kalenpw <kalenpwilliams@gmail.com>
2019-03-14 11:02:40 -0600

    Upload artwork completed

--- app/Http/Controllers/ArtworkController.php
+++ app/Http/Controllers/ArtworkController.php
@@ -2,21 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Http\Request;
 
 class ArtworkController extends Controller
 {
     public function store(Request $request)
     {
-        $request->file('artwork')->store('artworks');
-        return "succ";
-        return $request->file('artwork');
+        $requestToken = $request->token;
+        $token = JWTAuth::getToken();
+
+        if ($token == $requestToken) {
+            $apy = JWTAuth::getPayload($token)->toArray();
+            $userId = $apy['sub'];
+        } else {
+            return "Not validated";
+        }
         request()->validate([
             'name' => 'required',
-            'file' => 'required'
+            'artwork' => 'required',
+            'token' => 'required'
         ]);
-
+        $name = $request->name;
         $path = $request->file('artwork')->store('artworks');
-        return $path;
+        $game = \App\Game::where('name', $name)->first();
+        $gameId = $game->id;
+
+        return \App\Artwork::create([
+            'userId' => $userId,
+            'gameId' => $gameId,
+            'votes' => 0,
+            'fileName' => $path
+        ]);
     }
 }
